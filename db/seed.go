ed0ee20af7b23ca7c6e40cfbbbe454452e0d59da
uh-kay <150785230+uh-kay@users.noreply.github.com>
2025-11-25 02:43:27 +0700

    feat: seed comments

--- db/seed.go
+++ db/seed.go
@@ -10,7 +10,9 @@ import (
 func Seed(queries store.Storage) {
 	createAdminAccount(queries)
 	createUserAccount(queries)
+	createTags(queries)
 	createPosts(queries)
+	createComments(queries)
 }
 
 func createAdminAccount(queries store.Storage) {
@@ -96,18 +98,63 @@ func createPosts(queries store.Storage) {
 			Tags:     []string{"go", "tech"},
 		},
 		{
-			Title:    "Fedora 43 remove X11 support",
+			Title:    "Fedora 43 removed X11 support",
 			Content:  "Fedora team said they are removing X11 support in Fedora 43. This means you cannot use X11 anymore without compiling from source code.",
 			UserID:   2,
 			Username: "npc",
 			Tags:     []string{"fedora-linux", "linux"},
 		},
 	}
-	for i := range posts {
-		_, err := queries.Posts.Create(context.Background(), posts[i].Title, posts[i].Content, posts[i].UserID)
+	for _, post := range posts {
+		newPost, err := queries.Posts.Create(context.Background(), post.Title, post.Content, post.UserID)
+		if err != nil {
+			log.Println(err)
+		}
+
+		for _, tag := range post.Tags {
+			_, err := queries.PostTags.Create(context.Background(), newPost.ID, tag)
+			if err != nil {
+				log.Println(err)
+			}
+		}
+	}
+
+}
+
+func createTags(q store.Storage) {
+	tags := []string{"newsdrop", "go", "tech", "fedora-linux", "linux"}
+
+	for _, tag := range tags {
+		_, err := q.Tags.Create(context.Background(), tag)
 		if err != nil {
 			log.Println(err)
 		}
 	}
+}
 
+func createComments(q store.Storage) {
+	comments := []store.Comment{
+		{
+			PostID:  1,
+			UserID:  2,
+			Content: "Congratz!!",
+		},
+		{
+			PostID:  1,
+			UserID:  3,
+			Content: "I hope Newsdrop bring a positive impact to the world",
+		},
+		{
+			PostID:  2,
+			UserID:  1,
+			Content: "I'm excited to see how Green Tea garbage collector going to perform in real world scenario",
+		},
+	}
+
+	for _, comment := range comments {
+		_, err := q.Comments.Create(context.Background(), comment.Content, comment.UserID, comment.PostID)
+		if err != nil {
+			log.Println(err)
+		}
+	}
 }
